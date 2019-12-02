1f7fd0ba8a5a51b9baf6894020409fb967bd781c
Shirish Shrestha <shrestha_sl@hotmail.com>
2019-12-02 15:39:57 -0600

    ostrich added to the list

--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -4,23 +4,42 @@ using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class PurchaseStock {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Cow");
-            Console.WriteLine ("2. Ostrich");
+namespace Trestlebridge.Actions
+{
+    public class PurchaseStock
+    {
+        public static void CollectInput(Farm farm)
+        {
+            Console.WriteLine("1. Cow");
+            Console.WriteLine("2. Ostrich");
+            Console.WriteLine("3. Pig");
+            Console.WriteLine("4. Goat");
+            Console.WriteLine("5. Sheep");
 
-            Console.WriteLine ();
-            Console.WriteLine ("What are you buying today?");
+            Console.WriteLine();
+            Console.WriteLine("What are you buying today?");
 
-            Console.Write ("> ");
-            string choice = Console.ReadLine ();
+            Console.Write("> ");
+            string choice = Console.ReadLine();
 
             switch (Int32.Parse(choice))
             {
                 case 1:
                     ChooseGrazingField.CollectInput(farm, new Cow());
                     break;
+                case 2:
+                    ChooseGrazingField.CollectInput(farm, new Ostrich());
+                    break;
+                case 3:
+                    ChooseGrazingField.CollectInput(farm, new Pig());
+                    break;
+                case 4:
+                    ChooseGrazingField.CollectInput(farm, new Goat());
+                    break;
+                case 5:
+                    ChooseGrazingField.CollectInput(farm, new Sheep());
+                    break;
+
                 default:
                     break;
             }
