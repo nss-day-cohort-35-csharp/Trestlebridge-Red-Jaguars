3ae158edb0a5f339b879e00f0d897dcc65cdf236
stephensenft21 <stephensenft21@gmail.com>
2019-12-03 13:49:44 -0600

    fixed an error in choose naturaL field

--- src/Actions/ChooseNaturalField.cs
+++ src/Actions/ChooseNaturalField.cs
@@ -14,7 +14,7 @@ namespace Trestlebridge.Actions
 
             for (int i = 0; i < farm.NaturalFields.Count; i++)
             {
-                Console.WriteLine($"{i + 1}. Natural Field ({farm.NaturalFields[i].PlantsInFacility()} Plant(s) in the fiels)");
+                Console.WriteLine($"{i + 1}. Natural Field ({farm.NaturalFields[i].PlantsInFacility()} Plant(s) in the fields)");
                 farm.NaturalFields[i].PlantsGroups();
             }
 
