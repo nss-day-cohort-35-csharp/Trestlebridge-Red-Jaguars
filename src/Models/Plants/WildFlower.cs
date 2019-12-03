a3f8e48bb89b782c7f16ab2d358c48b6cd385fe0
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-12-03 09:42:32 -0600

    added WildFlower class

---
+++ src/Models/Plants/WildFlower.cs
@@ -0,0 +1,19 @@
+using System;
+using Trestlebridge.Interfaces;
+
+namespace Trestlebridge.Models.Plants
+{
+    public class WildFlower : IResource, ICompostProducing
+    {
+        private double _compostProduced = 30.3;
+        public string Type { get; } = "WildFlower";
+
+        public double Farmer () {
+            return _compostProduced;
+        }
+
+        public override string ToString () {
+            return $"WildFlower. Cows - Yum!";
+        }
+    }
+}
