e7454bd33239e2a8ae41c2e0bfe81cdae73e41db
stephensenft21 <stephensenft21@gmail.com>
2019-12-05 09:18:21 -0600

    added composter classes

---
+++ src/Models/Processors/Composter.cs
@@ -0,0 +1,47 @@
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Trestlebridge.Models;
+using Trestlebridge.Models.Facilities;
+using Trestlebridge.Interfaces;
+
+
+namespace Trestlebridge.Models.Processors {
+
+public class Composter {
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
