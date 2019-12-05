64d609e1ea0b43b19270332afd767a998cdc5cfe
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-12-05 10:12:10 -0600

    added message about amount of processed seeds

--- src/Actions/ChoosePlowedFieldForSeed.cs
+++ src/Actions/ChoosePlowedFieldForSeed.cs
@@ -16,82 +16,83 @@ namespace Trestlebridge.Actions
             {
 
                 Utils.Clear();
-                
+
                 var filterPlowedField = farm.PlowedFields.Where(field => field.PlantsInFacility() > 0).ToList();
                 if (filterPlowedField.Count > 0)
                 {
-                for (int i = 0; i < filterPlowedField.Count; i++)
-                {
-                    Console.WriteLine($"{i + 1}. Plowed Field ({filterPlowedField[i].PlantsInFacility()} Plant(s) in the fields)");
-                    filterPlowedField[i].PlantsGroups();
-                }
-
-                Console.WriteLine();
-
-                // How can I output the type of plant chosen here?
-                Console.WriteLine($"What field to harvest?");
-
-                Console.Write("> ");
-                int choice = Int32.Parse(Console.ReadLine());
-                Utils.Clear();
-                Console.Write("Heres a list of plants in your chosen field");
-                Console.WriteLine();
-                var plantsInFacility = filterPlowedField[choice - 1]._plants;
-                var selectedFacilityGroup = plantsInFacility.GroupBy(g => g.Type).ToList();
-
-                /*foreach (var plant in selectedFacilityGroup)
-                {
-                    Console.WriteLine($"This field has {plant.Count()} {plant.Key}(s)");
-                }*/
-
-                for (int i = 0; i < selectedFacilityGroup.Count; i++)
-                {
-                    Console.WriteLine($"{i+1}. {selectedFacilityGroup[i].Key} ({selectedFacilityGroup[i].Count()})");
-                }
-
-                Console.WriteLine();
-
-                // How can I output the type of plant chosen here?
-
-                Console.WriteLine($"What plant to process?");
-                Console.Write("> ");
-                int plantChoice = Int32.Parse(Console.ReadLine());
-                plantChoice--;
-
-                Console.WriteLine($"How many plants to process?");
-                Console.Write("> ");
-                int plantNumber = Int32.Parse(Console.ReadLine());
-
-                if (seedHarvester.GetFreeCapacity() >= plantNumber)
-                {
-                    for (int i = 0; i < plantNumber; i++)
+                    for (int i = 0; i < filterPlowedField.Count; i++)
                     {
-                        Console.WriteLine(selectedFacilityGroup[plantChoice].Last().ToString());
-                        seedHarvester.AddResource(selectedFacilityGroup[plantChoice].Last());
-                        plantsInFacility.Remove(plantsInFacility.Last(p => p.Type == selectedFacilityGroup[plantChoice].Key));
+                        Console.WriteLine($"{i + 1}. Plowed Field ({filterPlowedField[i].PlantsInFacility()} Plant(s) in the fields)");
+                        filterPlowedField[i].PlantsGroups();
+                    }
+
+                    Console.WriteLine();
+
+                    // How can I output the type of plant chosen here?
+                    Console.WriteLine($"What field to harvest?");
+
+                    Console.Write("> ");
+                    int choice = Int32.Parse(Console.ReadLine());
+                    Utils.Clear();
+                    Console.Write("Heres a list of plants in your chosen field");
+                    Console.WriteLine();
+                    var plantsInFacility = filterPlowedField[choice - 1]._plants;
+                    var selectedFacilityGroup = plantsInFacility.GroupBy(g => g.Type).ToList();
+
+                    /*foreach (var plant in selectedFacilityGroup)
+                    {
+                        Console.WriteLine($"This field has {plant.Count()} {plant.Key}(s)");
+                    }*/
+
+                    for (int i = 0; i < selectedFacilityGroup.Count; i++)
+                    {
+                        Console.WriteLine($"{i+1}. {selectedFacilityGroup[i].Key} ({selectedFacilityGroup[i].Count()})");
+                    }
+
+                    Console.WriteLine();
+
+                    // How can I output the type of plant chosen here?
+
+                    Console.WriteLine($"What plant to process?");
+                    Console.Write("> ");
+                    int plantChoice = Int32.Parse(Console.ReadLine());
+                    plantChoice--;
+
+                    Console.WriteLine($"How many plants to process?");
+                    Console.Write("> ");
+                    int plantNumber = Int32.Parse(Console.ReadLine());
+
+                    if (seedHarvester.GetFreeCapacity() >= plantNumber)
+                    {
+                        for (int i = 0; i < plantNumber; i++)
+                        {
+                            Console.WriteLine(selectedFacilityGroup[plantChoice].Last().ToString());
+                            seedHarvester.AddResource(selectedFacilityGroup[plantChoice].Last());
+                            plantsInFacility.Remove(plantsInFacility.Last(p => p.Type == selectedFacilityGroup[plantChoice].Key));
+
+                        }
+                        Console.WriteLine("Do you want to add more? Y/N");
+                        answer = Console.ReadLine();
 
                     }
-                    Console.WriteLine("Do you want to add more? Y/N");
-                    answer = Console.ReadLine();
-
+                    else
+                    {
+                        Console.WriteLine("No more space! Processing current resource.");
+                        Console.ReadLine();
+                        answer = "N";
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("No more space! Processing current resource.");
+                    Console.WriteLine("no plants in field, processing everything in harvester");
+
                     Console.ReadLine();
                     answer = "N";
-                }
-                } 
-                else {
-                    Console.WriteLine("no plants in field, processing everything in harvester");
-                    
-                    Console.ReadLine();
-                    answer= "N";
                 }
             } while (answer == "Y");
 
             seedHarvester.Process(farm);
-
+            Console.ReadLine();
 
             //plantsInFacility[plantChoice].Harvest();
 
@@ -100,7 +101,7 @@ namespace Trestlebridge.Actions
                 Stretch goal. Only if the app is fully functional.
              */
             // farm.PurchaseResource<IPlowed>(plant, choice);
-            
+
         }
     }
 }
